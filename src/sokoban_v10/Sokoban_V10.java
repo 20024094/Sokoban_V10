9518ace0dd4e83ab5fd1f37e627576e03e1b665a
danie <danie@Daniel.home>
2024-05-15 22:20:53 +0100

    OOP version 8 version Sokoban

--- src/sokoban_v10/Sokoban_V10.java
+++ src/sokoban_v10/Sokoban_V10.java
@@ -1,27 +1,36 @@
 package sokoban_v10;
 
 /**
+ * Module: Object Oriented Programming 
+ * Student: Daniel Barbu: 20024094
+ * Application: Sokoban 
+ * Version: 10.0 
+ * Date: 15/05/2024 
+ * File: Sokoban_V10.java
  *
- * @author danie
+ * @author Daniel Barbu
+ * @version 10.0
  */
 
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-
 
 public class Sokoban_V10 {
 
     public static void main(String[] args) {
+        // schedule a job for the event-dispatching thread creating and showing the GUI.
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Sokoban");
+            // a new instance of Game class
             Game sokobanGame = new Game();
-            
+
             // Polymorphic state
             Game game = new Game();
-            game.printAllElementStatuses(); // This will print the positions of all game elements
-            
+            game.printAllElementStatuses(); // print the positions of all game elements
+
             Dimension size = sokobanGame.calculateFrameSize();
 
             frame.setPreferredSize(size);
